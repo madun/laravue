940c516c046550541a307f663969dc78282c87e9
Ilman Manarul Qori <ilmankori@gmail.com>
2018-08-10 14:15:15 +0700

    Its Done

---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-warning mb-2">
+        <div class="container">
+            <a class="navbar-brand" href="">laraticles</a>
+        </div>
+    </nav>
+</template>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script>window.laravel = { csrfToken: '{{ csrf_token() }}' }</script>
+
+        <title>Laraarticle App</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+
+        <!-- Styles -->
+        <style>
+            hr {
+                margin-top: 1rem;
+                margin-bottom: 1rem;
+                border: 0;
+                border-top: 1px solid rgba(0, 0, 0, 0.1);
+            }
+        </style>
+    </head>
+    <body>
+        <div id="app">
+            <navbar></navbar>
+            <div class="container">
+                <articles></articles>
+            </div>
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
